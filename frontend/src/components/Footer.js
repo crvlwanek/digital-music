6234e2d80543219cb9e170515047bd6be00c9024
ChrisVL-W <chris.vanlanenwanek@gmail.com>
2021-02-03 17:28:49 -0600

    update footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, Grid, IconButton } from '@material-ui/core';
+import { Grid, Typography, Box } from '@material-ui/core';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import SocialIcon from "./SocialIcon";
@@ -7,6 +7,11 @@ import SocialIcon from "./SocialIcon";
 function Footer() {
     return (
         <footer>
+        <Box alignItems="center" justify="center">
+            <Typography align="center" className="footer-text">
+                Chris VL-Wanek © {new Date().getFullYear()}
+            </Typography>
+        </Box>
         <Grid className="social-text" container direction="row" justify="center" alignItems="center">
             <SocialIcon icon={GitHubIcon} link="https://github.com/crvlwanek/digital-music"/>
             <SocialIcon icon={LinkedInIcon} link="https://www.linkedin.com/in/chris-van-lanen-wanek-63b91514b/"/>
