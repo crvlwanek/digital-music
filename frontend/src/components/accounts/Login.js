b1f3ace2bc08a9b2faaa83ebee61ce4b0c54c292
ChrisVL-W <chris.vanlanenwanek@gmail.com>
2021-02-05 12:39:03 -0600

    Refactor login/register pages to material ui

--- frontend/src/components/accounts/Login.js
+++ frontend/src/components/accounts/Login.js
@@ -4,6 +4,14 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { login } from '../../actions/auth';
 
+import {
+  Typography, Avatar, 
+  TextField, Grid, CssBaseline,
+  Container, Button
+
+} from "@material-ui/core";
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+
 export class Login extends Component {
   state = {
     username: '',
@@ -23,50 +31,89 @@ export class Login extends Component {
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
   render() {
+    const classes = {
+      paper: {
+        marginTop: "24px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      },
+      avatar: {
+        margin: "8px",
+        backgroundColor: "blue",
+      },
+      form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: "8px",
+      },
+      submit: {
+        margin: "24px 0 16px",
+      },
+    }
+
     if (this.props.isAuthenticated) {
       return <Redirect to="/" />;
     }
     const { username, password } = this.state;
     return (
-      <div className="container">
-      <div className="col-md-6 m-auto">
-        <div className="card card-body mt-5">
-          <h2 className="text-center">Login</h2>
-          <form onSubmit={this.onSubmit}>
-            <div className="form-group">
-              <label>Username</label>
-              <input
-                type="text"
-                className="form-control"
-                name="username"
-                onChange={this.onChange}
-                value={username}
-              />
-            </div>
-
-            <div className="form-group">
-              <label>Password</label>
-              <input
-                type="password"
-                className="form-control"
-                name="password"
-                onChange={this.onChange}
-                value={password}
-              />
-            </div>
-
-            <div className="form-group">
-              <button type="submit" className="btn btn-primary">
-                Login
-              </button>
-            </div>
-            <p>
-              Don't have an account? <Link to="/register">Register</Link>
-            </p>
-          </form>
-        </div>
-      </div>
-      </div>
+      <Container component="main" maxWidth="xs">
+          <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Login
+            </Typography>
+            <form onSubmit={this.onSubmit} className={classes.form} noValidate>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                      autoComplete="username"
+                      name="username"
+                      variant="outlined"
+                      required
+                      fullWidth
+                      id="username"
+                      label="Username"
+                      onChange={this.onChange}
+                      value={username}
+                      autoFocus
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    variant="outlined"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    onChange={this.onChange}
+                    value={password}
+                  />
+                </Grid>
+              </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+              Login
+              </Button>
+              <Grid container justify="flex-end">
+                <Grid item>
+                  <Link to="/register" variant="body2">
+                    Don't have an account? Register
+                  </Link>
+                </Grid>
+              </Grid>
+            </form>
+          </div>
+        </Container>
     );
   }
 }
--- frontend/src/components/accounts/Register.js
+++ frontend/src/components/accounts/Register.js
@@ -5,6 +5,34 @@ import PropTypes from 'prop-types';
 import { register } from '../../actions/auth';
 import { createMessage } from '../../actions/messages';
 
+import {
+  Typography, Avatar, 
+  TextField, Grid, CssBaseline,
+  Container, Button
+
+} from "@material-ui/core";
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+
+const classes = {
+  paper: {
+    marginTop: "100px",
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: "8px",
+    backgroundColor: "blue",
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: "24px",
+  },
+  submit: {
+    margin: "24px 0 16px",
+  },
+}
+
 export class Register extends Component {
   state = {
     username: '',
@@ -41,63 +69,90 @@ export class Register extends Component {
     }
     const { username, email, password, password2 } = this.state;
     return (
-      <div className="container">
-      <div className="col-md-6 m-auto">
-        <div className="card card-body mt-5">
-          <h2 className="text-center">Register</h2>
-          <form onSubmit={this.onSubmit}>
-            <div className="form-group">
-              <label>Username</label>
-              <input
-                type="text"
-                className="form-control"
-                name="username"
-                onChange={this.onChange}
-                value={username}
-              />
-            </div>
-            <div className="form-group">
-              <label>Email</label>
-              <input
-                type="email"
-                className="form-control"
-                name="email"
-                onChange={this.onChange}
-                value={email}
-              />
-            </div>
-            <div className="form-group">
-              <label>Password</label>
-              <input
-                type="password"
-                className="form-control"
-                name="password"
-                onChange={this.onChange}
-                value={password}
-              />
-            </div>
-            <div className="form-group">
-              <label>Confirm Password</label>
-              <input
-                type="password"
-                className="form-control"
-                name="password2"
-                onChange={this.onChange}
-                value={password2}
-              />
-            </div>
-            <div className="form-group">
-              <button type="submit" className="btn btn-primary">
-                Register
-              </button>
-            </div>
-            <p>
-              Already have an account? <Link to="/login">Login</Link>
-            </p>
-          </form>
-        </div>
-      </div>
-      </div>
+      <Container component="main" maxWidth="xs">
+          <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Register
+            </Typography>
+            <form onSubmit={this.onSubmit} className={classes.form} noValidate>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                      autoComplete="username"
+                      name="username"
+                      variant="outlined"
+                      required
+                      fullWidth
+                      id="username"
+                      label="Username"
+                      onChange={this.onChange}
+                      value={username}
+                      autoFocus
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    variant="outlined"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    onChange={this.onChange}
+                    value={email}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    variant="outlined"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    onChange={this.onChange}
+                    value={password}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    variant="outlined"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Confirm Password"
+                    type="password"
+                    id="password2"
+                    onChange={this.onChange}
+                    value={password2}
+                  />
+                </Grid>
+              </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+              Register
+              </Button>
+              <Grid container justify="flex-end">
+                <Grid item>
+                  <Link to="/login" variant="body2">
+                    Already have an account? Login
+                  </Link>
+                </Grid>
+              </Grid>
+            </form>
+          </div>
+        </Container>
     );
   }
 }
